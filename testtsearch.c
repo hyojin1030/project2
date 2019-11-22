e84c04659b5332f6ea17a90b48da5acd61e32518
210 <210@localhost.localdomain>
2019-11-22 12:04:51 +0900

    ~15까지

---
+++ testtsearch.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+#include<search.h>
+
+struct node{
+	char *name;
+	int age;
+};
+
+#define TABLESIZE 50
+
+char nametable[TABLESIZE*20];
+struct node nodetable[TABLESIZE];
+struct node *root = NULL;
+
+int compare(const void *cp1, const void *cp2){
+	return strcmp(((struct node *)cp1)->name, ((struct node *)cp2)->name);
+}
+void print_node(const void *, VISIT, int);
+
+main(){
+	char *nameptr = nametable;
+	struct node *nodeptr = nodetable;
+	struct node **ret;
+	int i = 0;
+
+	while(scanf("%s%d",nameptr,&nodeptr->age)!=EOF&&i++<TABLESIZE){
+		nodeptr->name = nameptr;
+		ret = (struct node **) tsearch((void *)nodeptr, (void **) &root, compare);
+		printf("\"%s\"님이 ", (*ret) -> name);
+		if(*ret == nodeptr)
+			printf("트리에 추가되었습니다.\n");
+		else
+			printf("트리에 이미 존재합니다. \n");
+		nameptr += strlen(nameptr)+1;
+		nodeptr++;
+	}
+	twalk((void *) root, print_node);
+
+}
+
+void print_node(const void *nodeptr, VISIT order, int level){
+	if(order == preorder || order == leaf)
+		printf("이름  = %-20s, 나이 = %d\n",(*(struct node **)nodeptr)->name,(*(struct node **)nodeptr)->age);
+}
+
